2e8ff62927463735921bbff6f82cd55530ab4fff
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-02-19 21:25:33 +0100

    add a perf program for the old filters
    darcs-hash:20060219202533-2fc9d-5d64ab4ada1218e7e900b4d069c84f80ed593156.gz

---
+++ perf_old.cxx
@@ -0,0 +1,79 @@
+#include "itkImageFileReader.h"
+
+#include "itkBinaryDilateImageFilter.h"
+#include "itkBinaryErodeImageFilter.h"
+#include "itkBinaryBallStructuringElement.h"
+
+#include "itkTimeProbe.h"
+#include <vector>
+#include "itkMultiThreader.h"
+
+int main(int, char * argv[])
+{
+  itk::MultiThreader::SetGlobalMaximumNumberOfThreads(1);
+
+  const int dim = 3;
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim >    IType;
+  
+  // read the input image
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  
+  typedef itk::BinaryBallStructuringElement< PType, dim> KernelType;
+  KernelType kernel;
+  KernelType::SizeType kernelSize;
+  kernelSize.Fill( 20 );
+  kernelSize[2] = 7;
+  kernel.SetRadius(kernelSize);
+  kernel.CreateStructuringElement();
+   
+  typedef itk::BinaryDilateImageFilter< IType, IType, KernelType > DilateType;
+  DilateType::Pointer dilate = DilateType::New();
+  dilate->SetInput( reader->GetOutput() );
+  dilate->SetKernel( kernel );
+  
+  typedef itk::BinaryErodeImageFilter< IType, IType, KernelType > ErodeType;
+  ErodeType::Pointer erode = ErodeType::New();
+  erode->SetInput( reader->GetOutput() );
+  erode->SetKernel( kernel );
+  
+  reader->Update();
+  
+  
+  std::cout << "fg" << "\t" 
+            << "erode" << "\t" 
+            << "dilate" << "\t" 
+            << std::endl;
+
+  for( int v=100; v<=200; v+=100 )
+    {
+    dilate->SetForegroundValue( v );
+    erode->SetForegroundValue( v );
+
+    itk::TimeProbe etime;
+    itk::TimeProbe dtime;
+
+    for( int i=0; i<5; i++ )
+      {
+      etime.Start();
+      erode->Update();
+      etime.Stop();
+      erode->Modified();
+
+      dtime.Start();
+      dilate->Update();
+      dtime.Stop();
+      dilate->Modified();
+
+      }
+      
+    std::cout << v << "\t" 
+              << etime.GetMeanTime() << "\t" 
+              << dtime.GetMeanTime() << "\t" 
+              << std::endl;
+    }
+  return 0;
+}
+
